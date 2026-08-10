f5cd59a545e1cd596cc56054b480d3f2c13c6077
TALBIYA-PARVEEN <dev.talbiya.parveen@gmail.com>
2026-08-10 22:17:46 +0530

    Time: 75 ms (37.13%), Space: 78.1 MB (49.63%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,37 +1,27 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
         int n=heights.length;
-        int max=0;
-        int[] nse=next_smaller(heights,n);
-        int[] pse=previous_smaller(heights,n);
-        for(int i=0;i<n;i++){
-            int width=nse[i]-pse[i]-1;
-            max=Math.max(max,width*heights[i]);
-        }
-        return max;
-    }
-    public int[] next_smaller(int[] heights,int n){
+        int max_diff=0;
         Stack<Integer> stk=new Stack<>();
-        int[] ans=new int[n];
-        int idx=n-1;
-        for(int i=n-1;i>=0;i--){
-            while(!stk.isEmpty() && heights[stk.peek()]>=heights[i])stk.pop();
-            if(stk.isEmpty())ans[idx--]=n;
-            else ans[idx--]=stk.peek();
+        for(int i=0;i<n;i++){
+            while(!stk.isEmpty() && heights[stk.peek()]>heights[i]){
+                int element_idx=stk.peek();
+                stk.pop();
+                int nse=i;
+                int pse=(stk.isEmpty())?-1:stk.peek();
+                int width=nse-pse-1;
+                max_diff=Math.max(max_diff,width*heights[element_idx]);
+            }
             stk.push(i);
         }
-        return ans;
-    }
-    public int[] previous_smaller(int[] heights,int n){
-        Stack<Integer> stk=new Stack<>();
-        int[] ans=new int[n];
-        int idx=0;
-        for(int i=0;i<n;i++){
-            while(!stk.isEmpty() && heights[stk.peek()]>=heights[i])stk.pop();
-            if(stk.isEmpty())ans[idx++]=-1;
-            else ans[idx++]=stk.peek();
-            stk.push(i);
+        while(!stk.isEmpty()){
+            int element_idx=stk.peek();
+            stk.pop();
+            int pse=(stk.isEmpty())?-1:stk.peek();
+            int nse=n;
+            int width=nse-pse-1;
+            max_diff=Math.max(max_diff,width*heights[element_idx]);
         }
-        return ans;
+        return max_diff;
     }
 }
