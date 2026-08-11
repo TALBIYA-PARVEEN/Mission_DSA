1fead0db3147d2702d966722ad219f59cd333747
TALBIYA-PARVEEN <dev.talbiya.parveen@gmail.com>
2026-08-11 19:59:26 +0530

    Time: 7 ms (4.23%), Space: 48.4 MB (31.18%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,29 +1,21 @@
 class Solution {
     public int trap(int[] height) {
-        int n = height.length;
-        int[] p = pge(height, n);
-        int[] ne = nge(height, n);
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            int min = Math.min(p[i], ne[i]);
-            sum += min - height[i];
+        int n=height.length;
+        int sum=0;
+        Stack<Integer> stk=new Stack<>();
+        for(int i=0;i<n;i++){
+            while(!stk.isEmpty() && height[stk.peek()]<height[i]){
+                int height_val=height[stk.peek()];
+                stk.pop();
+                int left=0;
+                if(!stk.isEmpty())left=stk.peek();
+                else break;
+                int width=i-left-1;
+                int bounded=Math.min(height[left],height[i])-height_val;
+                sum+=width*bounded;
+            }
+            stk.push(i);
         }
         return sum;
-    }
-    public static int[] pge(int[] height, int n) {
-        int[] ans = new int[n];
-        ans[0] = height[0];
-        for (int i = 1; i < n; i++) {
-            ans[i] = Math.max(ans[i - 1], height[i]);
-        }
-        return ans;
-    }
-    public static int[] nge(int[] height, int n) {
-        int[] ans = new int[n];
-        ans[n - 1] = height[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            ans[i] = Math.max(ans[i + 1], height[i]);
-        }
-        return ans;
     }
 }
