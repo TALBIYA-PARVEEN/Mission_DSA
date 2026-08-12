93e9348d66c22db37ac4372da0bc265aed20258e
TALBIYA-PARVEEN <dev.talbiya.parveen@gmail.com>
2026-08-12 11:51:38 +0530

    Time: 1 ms (99.95%), Space: 80.4 MB (10.8%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max=nums[0];
+        int currsum=0;
+        for(int i=0;i<nums.length;i++){
+            currsum+=nums[i];
+            max=Math.max(max,currsum);
+            if(currsum<0){
+                currsum=0;
+                continue;
+            }
+        }
+        return max;
+    }
+}
