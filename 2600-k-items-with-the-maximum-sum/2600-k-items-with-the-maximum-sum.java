00b8665f8f8002cb33baa83ac074469cb92c5d4d
TALBIYA-PARVEEN <dev.talbiya.parveen@gmail.com>
2026-08-12 12:01:12 +0530

    Time: 1 ms (100%), Space: 43 MB (44.41%) - LeetHub

---
+++ 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        int sum=0;
+        int digit=1;
+        for(int i=0;i<k;i++){
+            if(numOnes>0){
+                sum+=1;
+                numOnes--;
+            }
+            else if(numZeros>0){
+                sum+=0;
+                numZeros--;
+            }
+            else{
+                sum+=-1;
+                numNegOnes--;
+            }
+        }
+        return sum;
+    }
+}
