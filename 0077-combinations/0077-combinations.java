d04305e4bc9113f9637452db4af20665ab454625
TALBIYA-PARVEEN <dev.talbiya.parveen@gmail.com>
2026-08-14 21:09:41 +0530

    Time: 13 ms (99.12%), Space: 98.1 MB (46.74%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> ans=new ArrayList<>();
+        recursion(1,k,new ArrayList<>(),ans,n);
+        return ans;
+    }
+    public void recursion(int idx,int k,ArrayList<Integer> temp,List<List<Integer>> ans,int n){
+        if(k==temp.size()){
+            ans.add(new ArrayList<Integer>(temp));
+            return;
+        }
+        for(int i=idx;i<=n;i++){
+            temp.add(i);
+            recursion(i+1,k,temp,ans,n);
+            temp.remove(temp.size()-1);
+        }
+    }
+}
