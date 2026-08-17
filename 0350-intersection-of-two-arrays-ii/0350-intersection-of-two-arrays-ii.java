7b382dc889032a5c7b424a03794eeeaed3d37970
TALBIYA-PARVEEN <dev.talbiya.parveen@gmail.com>
2026-08-17 10:50:45 +0530

    Time: 3 ms (95.17%), Space: 44.5 MB (94.06%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,20 +1,18 @@
+
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        int n1=nums1.length;
-        int n2=nums2.length;
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        int i=0,j=0;
+        HashMap<Integer,Integer> map =new HashMap<>();
+        int n=nums1.length;
         int idx=0;
-        int[] ans=new int[Math.min(n1,n2)];
-        while(i<n1 && j<n2){
-            if(nums1[i]==nums2[j]){
-                ans[idx++]=nums1[i];
-                i++;
-                j++;
+        int[] ans=new int[Math.min(nums1.length, nums2.length)];
+        for(int i=0;i<n;i++){
+            map.put(nums1[i],map.getOrDefault(nums1[i],0)+1);
+        }
+        for(int i=0;i<nums2.length;i++){
+            if(map.containsKey(nums2[i]) && map.get(nums2[i])!=0){
+                ans[idx++]=nums2[i];
+                map.put(nums2[i],map.getOrDefault(nums2[i],0)-1);
             }
-            else if(nums1[i]>nums2[j])j++;
-            else if(nums1[i]<nums2[j])i++;
         }
         return Arrays.copyOf(ans,idx);
     }
