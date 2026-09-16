8cbc7113d66d7e49e9ffd9767a960516efc20003
TALBIYA-PARVEEN <dev.talbiya.parveen@gmail.com>
2026-09-16 07:17:37 +0530

    Time: 1 ms (82.24%), Space: 44 MB (9.54%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -24,15 +24,15 @@ class Solution {
             int n=q.size();
             List<Integer> tempo=new ArrayList<>();
             for(int i=0;i<n;i++){
-                if(flag==1){
+                if(flag!=1){
                     TreeNode temp=q.pollLast();
-                    tempo.add(0, temp.val);
+                    tempo.add(temp.val);
                     if(temp.right!=null)q.offerFirst(temp.right);
                     if(temp.left!=null)q.offerFirst(temp.left);
                 }
                 else{
                     TreeNode temp=q.pollFirst();
-                    tempo.add(0, temp.val);
+                    tempo.add(temp.val);
                     if(temp.left!=null)q.offerLast(temp.left);
                     if(temp.right!=null)q.offerLast(temp.right);
                 }
